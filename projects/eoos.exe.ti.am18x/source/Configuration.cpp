d5f42a8b9650fd6553f777ee74dc2564855ec4cc
Sergey Baigudin <baigudin@mail.ru>
2017-11-17 10:39:03 +0300

    Source files for TI AM18x MCUs added

---
+++ projects/eoos.exe.ti.am18x/source/Configuration.cpp
@@ -0,0 +1,19 @@
+/** 
+ * The configuration of a target processor.
+ * 
+ * @author    Sergey Baigudin, sergey@baigudin.software
+ * @copyright 2016-2017, Embedded Team, Sergey Baigudin
+ * @license   http://embedded.team/license/ 
+ */
+#include "Configuration.hpp"
+ 
+/** 
+ * Constructor.
+ */   
+Configuration::Configuration() :
+    sourceClock (25000000),
+    cpuClock    (375000000),
+    heapAddr    (reinterpret_cast<void*>(0xffff0100)),
+    heapSize    (0x00001f00),
+    stackSize   (0x00000800){    
+}
