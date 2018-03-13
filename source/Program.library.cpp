7d5bdad47cd53e7ed2bb6fac8429be2721af35cc
Sergey Baigudin <baigudin@mail.ru>
2018-03-13 21:53:44 +0300

    Code is refactored

--- source/Program.library.cpp
+++ source/Program.library.cpp
@@ -6,10 +6,87 @@
  * @license   http://embedded.team/license/
  */
 #include "Program.hpp"
+
 #include "library.Align.hpp"
+#include "library.Buffer.hpp"
 
 namespace global
 {
+    using namespace library;
+
+    /**
+     * Tests the Align class interface.
+     */
+    template<typename T>
+    bool testAlign(const T& illegal)
+    {
+        Align<T> o1(5U);
+        Align<T> o2(o1);
+        Align<T> o3;        
+        if( o2.equal(o1) || o2 == o3 || o1 != o2 )
+        {
+            if( o3.equal(7) )
+            {
+                o3 = 15;
+            }
+            o2 = o3;
+        }
+        bool res;
+        const T v = o1;
+        if( v == illegal )
+        {
+            res = false;
+        }
+        else
+        {
+            res = true;
+        }
+        return res;
+    }
+    
+    /**
+     * Tests the Buffer class interface.
+     */
+    template<typename T>
+    T testIllegalValue(api::IllegalValue<T>& obj, const T& val)
+    {
+        if( obj.isIllegal(val) == true )
+        {
+            obj.setIllegal(val);            
+        }
+        return obj.getIllegal();
+    }
+    
+    /**
+     * Tests the Buffer class interface.
+     */
+    template<typename T>
+    bool testBuffer(const T& illegal)
+    {
+        T buf[7];
+        // Static buffer class        
+        const Buffer<T,7> bs0;         
+        const Buffer<T,5> bs1(illegal);
+        // Dynamic buffer class.
+        const Buffer<T,0> bd0(7);
+        const Buffer<T,0> bd1(7, illegal);        
+        const Buffer<T,0> bd2(7, buf);
+        Buffer<T,0> bd3(7, buf, illegal);        
+          /*     
+        const T ill = testIllegalValue<T>(bd3, 3);
+        if(ill == illegal)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        */
+        return false;
+    }
+    
+
     /**
      * Starts executing an operating system main program.
      *
@@ -27,6 +104,20 @@ namespace global
      */   
     int32 Program::start()
     {  
+        do
+        {
+            // Test the Align interface
+            if( not testAlign<uint32>(7U) )
+            {
+                break;
+            }
+            // Test the Buffer interface            
+            if( not testBuffer<int32>(-1) )
+            {
+                break;
+            }
+        }
+        while(false);
         return 0;
     }
 }
--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,96 @@
  */
 #include "Program.hpp"
 
+/*
+class A
+{
+
+public:
+
+    A()
+    {
+    }
+    
+    virtual ~A()
+    {
+    }
+    
+    void add(int v)
+    {
+        int sum = getSum();
+        sum += v;
+        setSum(sum);
+    }
+    
+private:
+
+    virtual int getSum() = 0;
+    
+    virtual void setSum(int sum) = 0;    
+    
+};
+
+class B : public A
+{
+    
+public:
+    
+    B() :
+        sum_ (0){
+    }
+    
+    virtual ~B()
+    {
+    }
+    
+private:
+
+    virtual int getSum()
+    {
+        return sum_;
+    }
+    
+    virtual void setSum(int sum)
+    {
+        sum_ = sum;
+    }
+    
+    int sum_;
+};
+
+class C : public B
+{
+    
+public:
+    
+    C() : B()
+    {
+    }
+    
+    virtual ~C()
+    {
+    }
+    
+    void add(int v)
+    {
+        int sum = getSum();
+        sum += v;
+        setSum(sum);
+    }    
+    
+private:
+    
+    virtual int getSum()
+    {
+        return 6;
+    }   
+    
+    virtual void setSum(int sum)
+    {
+    }    
+
+};
+*/
 /**
  * The main function.
  * 
@@ -21,6 +111,9 @@
  */   
 int main()
 {
+    //B  b;
+    //B& br = b;    
+    //br.add(7);
     return ::global::Program::start();
 }
 
